de667fbe796bc6bf92d317bee60bef8fda9f4a3b
thanh <lmthanh203@gmail.com>
2023-10-16 10:36:04 +0700

    Cập nhập giao diện

--- app/src/main/java/com/example/appnew/MainActivity.java
+++ app/src/main/java/com/example/appnew/MainActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.appnew.adapter.ItemCallback;
+
+public class MainActivity extends AppCompatActivity implements ItemCallback {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onItemClick(String id) {
+
     }
 }
